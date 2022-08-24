72cffcaca5ae2127dce465d80ea91c99062419ac
JoaquinRiv <90224914+JoaquinRiv@users.noreply.github.com>
2022-08-23 22:09:11 -0400

    Add files via upload

---
+++ Clase3_Condicionees1/src/clase3_condicionees1/Clase3_Condicionees1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3_condicionees1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CETECOM
+ */
+public class Clase3_Condicionees1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+//        int mes = 0;
+//        String estacion = "";
+//        
+//        if (mes == 12 || mes == 1 || mes == 2){
+//        estacion = "Verano";
+//        System.out.println(estacion+" es la estacion actual");
+//        
+//        }else if(mes ==3 || mes == 4 || mes ==5){
+//        estacion = "Otoño";
+//        System.out.println(estacion+" es la estacion actual");
+//        
+//        
+//        }else if (mes ==6 || mes == 7 || mes ==8){
+//        estacion = "Invierno";
+//        System.out.println(estacion+" es la estacion actual");
+//        
+//        }else if (mes ==9 || mes == 10 || mes ==11){
+//        estacion = "Primavera";
+//        System.out.println(estacion+" es la estacion actual");
+//        }
+
+//       Scanner opcion = new Scanner(System.in);
+//        System.out.println("selecciona un numero");
+//        int numero = opcion.nextInt();
+//        if(numero >0 && numero <11){
+//            if (numero%2==0){
+//                System.out.println("el numero: "+numero+" es par");
+//            }else{
+//                System.out.println("el numero: "+ numero + " es impar");
+//            }
+//        }else{
+//            System.out.println("el numero: "+ numero +" no se encuentra en el rango");
+//        }
+
+
+    }  
+}
